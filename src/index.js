a6bdfc2d0e24fa005388a7efcf0e55acd554e6a3
Angel Beltran <angel.beltran@tcnbroadcasting.com>
2017-05-16 17:09:01 -0600

    sample file

--- src/index.js
+++ src/index.js
@@ -1,69 +1,22 @@
-import { take, put, call, fork, select, all } from 'redux-saga/effects'
+const SagaPlugin = require('./SagaPlugin')
 
-/**
- * Some saga
- * @param {string} someArg1 - some string
- * @param {number} someArg2 - some number
- * @param {boolean} someArg3 - some boolean
- * @sagaselect someSubstate - some part of the state
- * @sagatake SOME_ACTION_TYPE - some action type
- * @sagacall someApiCall - some api call
- */
-export function* someSaga (someArg1, someArg2, someArg3) {
-  const something = yield select((state) => state.someSubstate)
 
-  return !!something.something
-}
+const sagaPlugin = new SagaPlugin()
 
-/**
- * @param {string} anotherArg1 - another string
- * @param {number} anotherArg2 - another number
- * @param {boolean} anotherArg3 - another boolean
- * @sagaselect anotherSubstate - another part of the state
- * @sagaput ANOTHER_ACTION_TYPE - another action type
- * @sagacall anotherApiCall - another api call
- */
-function* anotherSaga (anotherArg1, anotherArg2, anotherArg3) {
-  const anotherThing = yield select((state) => state.anotherSubstate)
+module.exports.onHandleContent = function(ev) {
+  ev.data.content = sagaPlugin.exec(ev.data.content)
+};
 
-  return !!anotherThing.anotherThing
-}
-
-/**
- * A saga with no params
- * @sagaselect anotherSubstate - another part of the state
- * @sagaput ANOTHER_ACTION_TYPE - another action type
- * @sagacall anotherApiCall - another api call
- */
-function* noParamsSaga () {
-  const whatever = yield select((state) => state.whatever)
-
-  return !!whatever.whatever
-}
-
-/**
- * @example const inst = new SomeClass(val)
- */
-class SomeClass {
-  /**
-   * @param {string} val - some string value
-   */
-  constructor (val) {
-    /**
-     * @type {string} SomeClass#val
-     */
-    this.val = val
-  }
-
-  /**
-   * @param {string} someParam - some string parameter
-   */
-  someMethod (someParam) {
-    console.log(someParam)
-  }
-}
-
-export default {
-  someSaga,
-  SomeClass,
-}
+module.exports.onHandleDocs = function(ev) {
+  ev.data.docs.filter(d => d.kind === 'function').forEach((d) => {
+    if (d.unknown) {
+      for (let i = 0; i < d.unknown.length; i++) {
+        const unknownTag = d.unknown[i].tagName
+        if (unknownTag.indexOf('@saga') === 0) {
+          sagaPlugin.push(d)
+          break
+        }
+      }
+    }
+  })
+};
---
+++ src/sample.js
@@ -0,0 +1,69 @@
+import { take, put, call, fork, select, all } from 'redux-saga/effects'
+
+/**
+ * Some saga
+ * @param {string} someArg1 - some string
+ * @param {number} someArg2 - some number
+ * @param {boolean} someArg3 - some boolean
+ * @sagaselect someSubstate - some part of the state
+ * @sagatake SOME_ACTION_TYPE - some action type
+ * @sagacall someApiCall - some api call
+ */
+export function* someSaga (someArg1, someArg2, someArg3) {
+  const something = yield select((state) => state.someSubstate)
+
+  return !!something.something
+}
+
+/**
+ * @param {string} anotherArg1 - another string
+ * @param {number} anotherArg2 - another number
+ * @param {boolean} anotherArg3 - another boolean
+ * @sagaselect anotherSubstate - another part of the state
+ * @sagaput ANOTHER_ACTION_TYPE - another action type
+ * @sagacall anotherApiCall - another api call
+ */
+function* anotherSaga (anotherArg1, anotherArg2, anotherArg3) {
+  const anotherThing = yield select((state) => state.anotherSubstate)
+
+  return !!anotherThing.anotherThing
+}
+
+/**
+ * A saga with no params
+ * @sagaselect anotherSubstate - another part of the state
+ * @sagaput ANOTHER_ACTION_TYPE - another action type
+ * @sagacall anotherApiCall - another api call
+ */
+function* noParamsSaga () {
+  const whatever = yield select((state) => state.whatever)
+
+  return !!whatever.whatever
+}
+
+/**
+ * @example const inst = new SomeClass(val)
+ */
+class SomeClass {
+  /**
+   * @param {string} val - some string value
+   */
+  constructor (val) {
+    /**
+     * @type {string} SomeClass#val
+     */
+    this.val = val
+  }
+
+  /**
+   * @param {string} someParam - some string parameter
+   */
+  someMethod (someParam) {
+    console.log(someParam)
+  }
+}
+
+export default {
+  someSaga,
+  SomeClass,
+}
